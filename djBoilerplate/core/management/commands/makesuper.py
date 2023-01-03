2ac5a67c9b3a7b68fe0ad04026246a5452c6349a
Harjot Barn <harjotba@buffalo.edu>
2023-01-02 23:07:29 -0500

    added email for makesuper

--- djBoilerplate/core/management/commands/makesuper.py
+++ djBoilerplate/core/management/commands/makesuper.py
@@ -7,13 +7,14 @@ class Command(BaseCommand):
         User = get_user_model()
         username_ = settings.CONFIG_YAML["App"]["superuser_username"]
         password_= settings.CONFIG_YAML["App"]["superuser_password"]
+        email_= settings.CONFIG_YAML["App"]["superuser_email"]
         if not User.objects.filter(username=username_).exists():
             #  change the username, email and password before use
             #   it is recomended to use environment variables for this to avoid pushing sensitive 
             #      info in remote repository
             
-            User.objects.create_superuser(username_, "admin@xyzdomain.com", password_)
-            s = "'"+username_ +"' user has created with password '"+password_+"'"
+            User.objects.create_superuser(username_, email_, password_)
+            s = "'"+username_ +"' user has created with password '"+password_+"' and email "+email_
             self.stdout.write(self.style.SUCCESS(s))
         else:
             self.stdout.write(self.style.SUCCESS("admin user already exists"))
